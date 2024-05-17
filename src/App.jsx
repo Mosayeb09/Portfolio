4d69a41dfa63fe13625f9e9c62ea2a8c44004c64
Mosayeb09 <mosayebhossain09@gmail.com>
2024-05-17 18:12:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./Component/Hero/Hero";
+import Nav from "./Component/Nav/Nav";
+// import Navbar from "./Component/Navbar_/Navbar";
+// import Navbar from "./Component/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+       <Nav></Nav>
+       <Hero></Hero>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Component/Nav/Nav.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-scroll';
+import './Nav.css'
+
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+              <li><Link to='home'>Home</Link></li>  
+                
+
+                
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
