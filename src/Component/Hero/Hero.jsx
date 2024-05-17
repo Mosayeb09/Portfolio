4585e29d5d04ce9350927f135d64741801381386
Mosayeb09 <mosayebhossain09@gmail.com>
2024-05-18 00:53:55 +0600

    working on hero section

--- src/Component/Hero/Hero.jsx
+++ src/Component/Hero/Hero.jsx
@@ -1,10 +1,33 @@
 import './Hero.css'
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { RiLinkedinBoxFill } from "react-icons/ri";
+import { BsGithub } from "react-icons/bs";
 
 const Hero = () => {
     return (
-        <div>
-            <h1>hello</h1>
+        <div className='hero-container'>
+        <div className="hero-text">
+            <h3>Hi, Myself</h3>
+            <h1>Mosayeb Hossain</h1>
+            <h3>And I'm a <span className='dev'>Frontend Developer</span></h3>
+            <p>Enthusiastic entry-level Web Developer with a strong skill set, innovative mindset, and proficiency
+                in web development languages and frameworks. Seeking a role within a dynamic team to drive
+                advancements in web development and foster professional growth.</p>
+                <div className="social-media">
+                    <a href="">{<FaFacebook />} </a>
+                    <a href="">{<FaSquareInstagram />} </a>
+                    <a href="">{<RiLinkedinBoxFill />} </a>
+                    <a href="">{<BsGithub />} </a>
+                </div>
+                
+                <a href=""><button className='btn'>Download Cv</button></a>
         </div>
+        <div className="hero-img">
+
+        </div>
+
+    </div>
     );
 };
 
--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -1,16 +1,35 @@
 import { Link } from 'react-scroll';
 import './Nav.css'
+import { useEffect, useState } from 'react';
+import menu_icon from '../../../public/Portfolio_assets/menu-icon.png'
 
 
 const Nav = () => {
+    const [sticky, setSticky]=useState(false)
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            window.scrollY > 50 ? setSticky(true) : setSticky(false)
+        })
+    }, [])
+
     return (
-        <nav>
-            <ul>
-              <li><Link to='home'>Home</Link></li>  
-                
+        <nav className={`container ${sticky ? 'dark-nav' : ''} `}>
+             <a href="hero" className='ptflo' >Portfolio.</a>
+
+            
+             <ul>
+                <li><Link to={'home'} >Home</Link></li>
+                <li><Link to={'About'}>About</Link> </li>
+                <li> <Link to={'portfolio'}>Portfolio</Link></li>
+                <li> <Link to={'contact'}>Contact</Link></li>
+                <li></li>
+            </ul>
+  
+            <img src={menu_icon} alt="" />
+   
 
                 
-            </ul>
+           
         </nav>
     );
 };
